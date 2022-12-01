78d88ca918bd12784769e0bd94cd1afa77842231
Shahrin Khan <76486119+Shahrin-khan@users.noreply.github.com>
2022-12-01 11:21:11 +0600

    Add files via upload

---
+++ socket program1.py
@@ -0,0 +1,9 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print ("Socket successfully created")
+# default port for socket
+port=80
+host_ip = socket.gethostbyname('www.google.com')
+# connecting to the server
+s.connect((host_ip, port))
+print ('IP of www.google.com',host_ip)
